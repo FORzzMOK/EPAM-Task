43d0d4eb8073008ea53b11afd766414a210ca97b
Pavel Nelubov <49573947+FORzzMOK@users.noreply.github.com>
2019-08-19 17:53:34 +0400

    Add files via upload

---
+++ Task_06/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Users_and_awards.BLL;
+
+namespace Users_and_awards.PL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            SelectOptionByUser();
+            //Console.ReadLine();
+        }
+
+        private static void SelectOptionByUser()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please, select some action");
+                Console.WriteLine("1: Add User");
+                Console.WriteLine("2: Delete User");
+                Console.WriteLine("3: Show User's info");
+                Console.WriteLine("4: Show User's list");
+                Console.WriteLine("5: Add awards");
+                Console.WriteLine("6: Exit");
+
+                var input = Console.ReadLine();
+
+                if (uint.TryParse(input, out uint selectedOptions)
+                    && selectedOptions > 0
+                    && selectedOptions < 7)
+                {
+                    int usersId = -1;
+                    switch (selectedOptions)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter user's name");
+                            string usersName = Console.ReadLine();
+                            Console.WriteLine("Enter user's date of birth in the format YYYY,MM,DD");
+                            DateTime dateOfBirth = new DateTime(1995, 01, 01);
+                            DateTime.TryParse(Console.ReadLine(), out dateOfBirth);
+                            UsersManager.CreateNewUser(usersName, dateOfBirth);
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter user's id. if you do not remember id, you can look at the list.");
+                            int.TryParse(Console.ReadLine(), out usersId);
+                            if (UsersManager.DeleteUser(usersId)) Console.WriteLine("True");
+                            else Console.WriteLine("False");
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter user's id. if you do not remember id, you can look at the list");
+                            int.TryParse(Console.ReadLine(), out usersId);
+                            Console.WriteLine(UsersManager.ShowUser(usersId));
+                            break;
+                        case 4:
+                            Console.WriteLine(UsersManager.ShowUsersList());
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter user's id. if you do not remember id, you can look at the list");
+                            int.TryParse(Console.ReadLine(), out usersId);
+                            Console.WriteLine("Enter user's award");
+                            string usersaward = Console.ReadLine();
+                            if (UsersManager.AppendAwards(usersId, usersaward)) Console.WriteLine("True");
+                            else Console.WriteLine("False");
+                            break;
+                        case 6:
+                            UsersManager.DataSynchronization();
+                            return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Task_06/UsersData.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsersEntities;
+using System.IO;
+using System.Globalization;
+
+namespace Users_and_awards.DAL
+{
+    public class UsersData
+    {
+        static UsersData()
+        {
+            DataSynchronizationStart();
+        }
+        private static List<User> UsersList = new List<User>();
+        public static void AddUser(User newUser)
+        {
+            UsersList.Add(newUser);
+            File.AppendAllText("MyData.txt", $"{newUser.ToString()} \n");
+        }
+        public static bool RemoveUser(int usersId)
+        {
+            User myUser = GetUserById(usersId);
+            if (myUser != null)
+            {
+                //string line = "";
+                //using (StreamReader reader = new StreamReader("MyData.txt"))
+                //{
+                //    using (StreamWriter writer = new StreamWriter("MyDataNew.txt"))
+                //    {
+                //        while ((line = reader.ReadLine()) != null)
+                //        {
+                //            if (line.StartsWith($"{usersId}")) continue;
+                //            writer.WriteLine(line);
+                //        }
+                //    }
+                //}
+                UsersList.Remove(myUser);
+                return true;
+            }
+            else return false;
+
+        }
+        public static int GetId()
+        {
+            int newId = -1;
+            foreach (User item in UsersList)
+            {
+                if (item.Id > newId) newId = item.Id;
+            }
+            return newId + 1;
+        }
+        public static User GetUserById(int usersId)
+        {
+            foreach (User item in UsersList)
+            {
+                if (item.Id == usersId)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        public static string GetUsersList()
+        {
+            StringBuilder UsersListString = new StringBuilder();
+            UsersListString.Append($"id    Name    Date of birth   Age   Awards  \n");
+            foreach (User item in UsersList)
+            {
+                UsersListString.Append($"{item.ToString()} \n");
+            }
+            return UsersListString.ToString();
+        }
+        public static void DataSynchronizationStart()
+        {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            string format = "yyyy,MM,dd";
+            if (!File.Exists("MyData.txt"))
+            {
+                File.Create("MyData.txt");
+            }
+            else
+            {
+                foreach (string line in File.ReadLines("MyData.txt"))
+                {
+                    string[] myStringArray = line.Split(' ');
+                    User newUser = new User
+                    {
+                        Name = myStringArray[1],
+                        DateOfBirth = DateTime.ParseExact(myStringArray[2], format, provider)
+                    };
+                    int.TryParse(myStringArray[0], out int id);
+                    newUser.Id = id;
+                    UsersList.Add(newUser);
+                    for(int i = 3; i < myStringArray.Length; i++)
+                    {
+                        newUser.AddAward(myStringArray[i]);
+                    }
+                }
+            }
+        }
+        public static void DataSynchronizationEnd()
+        {
+            File.Delete("MyData.txt");
+            foreach (User item in UsersList)
+            {
+                File.AppendAllText("MyData.txt", $"{item.ToString()} \n");
+            }
+        }
+    }
+}
