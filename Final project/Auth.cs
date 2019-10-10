f71c78ae89346274a43031b2cd610c96aebb696c
Pavel Nelubov <49573947+FORzzMOK@users.noreply.github.com>
2019-10-10 21:18:38 +0400

    Add files via upload

---
+++ Final project/Auth.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Security;
+using Users.DAL;
+namespace Great_Album
+{
+    public static class Auth
+    {
+        public static bool CanLogin(string login, string password)
+        {
+            UserMemoryStorage newStorage = new UserMemoryStorage();
+            return newStorage.CheckLoginAndPass(login, password);
+        }
+    }
+}
